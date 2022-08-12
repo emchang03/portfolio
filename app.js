ed1d72285bd5c5d65ef437aed83162006755dd5b
Emily Chang <emilychang@Emilys-MacBook-Pro-2.local>
2022-08-11 19:50:47 -0700

    heroku

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ app.get("/clubnext", function(req,res){
 });
 
 // set up the server 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
-  });
-  
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
+  console.log("Server successfully");
+});
